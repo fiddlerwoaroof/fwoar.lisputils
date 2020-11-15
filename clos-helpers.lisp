d5ed16e3f7b9b86f072f48e41b6fc8e703b506ba
Ed L <el-github@elangley.org>
2020-11-14 17:25:08 -0800

    feat: generate optional arguments in clos-helpers

--- clos-helpers.lisp
+++ clos-helpers.lisp
@@ -22,27 +22,58 @@
                                   initializer-syms)))))
 
 (defmacro defclass+ (name (&rest super) &body (direct-slots &rest options))
-  (let ((initargs (append (mapcan (lambda (class)
-                                    (typecase class
-                                      (cons (cadr class))
-                                      (t nil)))
-                                  super)
-                          (mapcan (lambda (slot)
-                                    (alexandria:ensure-list
-                                     (alexandria:when-let ((initarg (getf (cdr slot)
-                                                                          :initarg)))
-                                       (make-symbol (symbol-name initarg)))))
-                                  direct-slots))))
-    `(progn (defclass ,name
-                ,(mapcar (lambda (it)
-                           (typecase it
-                             (cons (car it))
-                             (t it)))
-                  super)
-              ,direct-slots
-              ,@options)
-            (defun ,name (,@initargs)
-              (fw.lu:new ',name ,@initargs)))))
+  (let* ((initargs (append (mapcan (lambda (class)
+                                     (typecase class
+                                       (cons (mapcar (lambda (it)
+                                                       (list it nil))
+                                                     (cadr class)))
+                                       (t nil)))
+                                   super)
+                           (mapcan (lambda (slot)
+                                     (alexandria:ensure-list
+                                      (alexandria:when-let ((initarg (getf (cdr slot)
+                                                                           :initarg)))
+                                        (fw.lu:prog1-bind
+                                            (it (list
+                                                 (list (make-symbol (symbol-name initarg))
+                                                       (eq :missing
+                                                           (getf (cdr slot)
+                                                                 :initform
+                                                                 :missing)))))))))
+                                   direct-slots))))
+    (destructuring-bind (required optional)
+        (loop for it in initargs
+              if (second it) collect (first it) into required
+                else collect (first it) into optional
+              finally (return (list required
+                                    optional)))
+      (let ((passed-args (mapcar (lambda (it)
+                                   (make-symbol (concatenate 'string
+                                                             (symbol-name it)
+                                                             "-P")))
+                                 optional)))
+        `(progn (defclass ,name
+                    ,(mapcar (lambda (it)
+                               (typecase it
+                                 (cons (car it))
+                                 (t it)))
+                      super)
+                  ,direct-slots
+                  ,@options)
+                (defun ,name (,@required ,@(when optional
+                                             (list* '&optional
+                                                    (mapcar (lambda (it it-p)
+                                                              `(,it nil ,it-p))
+                                                            optional
+                                                            passed-args))))
+                  ,(if optional
+                       (let ((heads (reverse (inits optional))))
+                         `(cond ,@(mapcar (lambda (it it-p)
+                                            `(,it-p (fw.lu:new ',name ,@required ,@it)))
+                                          heads
+                                          passed-args)
+                                (t (fw.lu:new ',name ,@required))))
+                       `(fw.lu:new ',name ,@required ,@optional))))))))
 
 (defun-ct %constructor-name (class)
   (format nil "~a-~a" '#:make class))
